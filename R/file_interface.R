6e6f720beababb185513ee61c8d3a63e0507ffb7
Jonathan Sulc <jonsulc@gmail.com>
2024-11-29 14:53:22 +0100

    Add compiling of multiple conditions and initial file reading

--- R/file_interface.R
+++ R/file_interface.R
@@ -10,6 +10,7 @@ new_file_interface <- function(
                           class    = c("file_interface", "character"))
   finterface$values_are_quoted <- are_values_quoted(finterface)
   finterface$column_indices <- get_column_names(finterface)
+  finterface$values_are_comma_separated <- is_file_csv(finterface)
   finterface
 }
 
@@ -45,6 +46,15 @@ are_values_quoted <- function(
     names() |>
     stringr::str_detect("\"") |>
     all()
+}
+
+is_file_csv <- function(
+  finterface
+) {
+  dt_output <- head(finterface, nlines = 1, verbose = TRUE) |>
+    capture.output() |>
+    stringr::str_match("sep='([^']+)'")
+  dt_output[!is.na(dt_output[, 1]), 2][1] == ","
 }
 
 get_column_names <- function(
@@ -96,7 +106,8 @@ add_condition <- function(
 
   eval(rlang::enexpr(conditions),
        e) |>
-    purrr::list_flatten()
+    purrr::list_flatten() |>
+    as_cmd(finterface = finterface)
 }
 
 #' @export
@@ -111,4 +122,41 @@ print.file_interface <- function(
               finterface$gzipped,
               finterface$values_are_quoted),
       "\n")
+}
+
+as_cmd <- function(
+  conditions_list,
+  finterface
+) {
+  wrap_initial_condition(conditions_list[[1]], finterface) |>
+    list() |>
+    c(lapply(conditions_list[-1], wrap_non_initial_condition, finterface)) |>
+    paste(collapse = " | ")
+}
+
+wrap_initial_condition <- function(
+  single_condition,
+  finterface
+) {
+  if (finterface$gzipped) {
+    return(
+      sprintf("zcat %s | awk%s '%s'",
+              finterface$filename,
+              ifelse(finterface$values_are_comma_separated, " -F',", ""),
+              single_condition)
+    )
+  }
+  sprintf("awk%s '%s' %s",
+          ifelse(finterface$values_are_comma_separated, " -F',", ""),
+          single_condition,
+          finterface$filename)
+}
+
+wrap_non_initial_condition <- function(
+  single_condition,
+  finterface
+) {
+  sprintf("awk%s '%s'",
+          ifelse(finterface$values_are_comma_separated, " -F',", ""),
+          single_condition)
 }
